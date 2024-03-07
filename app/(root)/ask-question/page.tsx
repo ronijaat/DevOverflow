7655b9bd442105634d1a47e004e691a850b159d5
ronijaat <raunakbaliyan01@gmail.com>
2024-03-07 10:14:18 +0530

    fetching questions on home page

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,5 +1,27 @@
-const AskQuestion = () => {
-  return <div>AskQuestion</div>;
+import Question from '@/components/forms/Question';
+import User from '@/database/user.model';
+import { connectToDatabase } from '@/lib/mongoose';
+import { redirect } from 'next/navigation';
+
+const AskQuestion = async () => {
+  const userId = '123456789';
+  if (!userId) redirect('/sign-in');
+
+  connectToDatabase();
+
+  const mongoUser = await User.findOne({ clerkid: userId });
+
+  console.log(mongoUser);
+
+  return (
+    <div>
+      <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
+
+      <div className="mt-9">
+        <Question mongoUserId={JSON.stringify(mongoUser._id)} />
+      </div>
+    </div>
+  );
 };
 
 export default AskQuestion;
