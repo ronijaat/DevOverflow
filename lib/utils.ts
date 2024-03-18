9548fcc1f7e4057bc9fc8324ddb5e7185793d57b
ronijaat <raunakbaliyan01@gmail.com>
2024-03-18 17:11:53 +0530

    badge

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,6 +2,9 @@ import { clsx, type ClassValue } from 'clsx';
 import qs from 'query-string';
 import { twMerge } from 'tailwind-merge';
 
+import { BADGE_CRITERIA } from '@/constants';
+import { BadgeCounts } from '@/types';
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
@@ -108,4 +111,34 @@ export const removeKeysFromQuery = ({
     },
     { skipNull: true }
   );
+};
+
+interface BadgeParam {
+  criteria: {
+    type: keyof typeof BADGE_CRITERIA;
+    count: number;
+  }[];
+}
+
+export const assignBadges = (params: BadgeParam) => {
+  const badgeCounts: BadgeCounts = {
+    GOLD: 0,
+    SILVER: 0,
+    BRONZE: 0,
+  };
+
+  const { criteria } = params;
+
+  criteria.forEach((item) => {
+    const { type, count } = item;
+    const badgeLevels: any = BADGE_CRITERIA[type];
+
+    Object.keys(badgeLevels).forEach((level: any) => {
+      if (count >= badgeLevels[level]) {
+        badgeCounts[level as keyof BadgeCounts] += 1;
+      }
+    });
+  });
+
+  return badgeCounts;
 };
